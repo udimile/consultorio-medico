43dd0766a1fa587addd7031605256399253c56ed
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2024-11-16 13:43:52 -0300

    implementação das classes Pessoa, Paciente e Medico.

---
+++ src/models/Pessoa.java
@@ -0,0 +1,81 @@
+package models;
+
+import java.util.Date;
+
+//classe abstrata que define atributos comuns a médico e paciente
+public abstract class Pessoa {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private Date dataNascimento; // do tipo dd/mm/yy
+    private Date dataCadastro; // do tipo dd/mm/yy
+
+    public Pessoa(String nome, String cpf, String email, String telefone, String endereco, Date dataNascimento, Date dataCadastro) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.dataNascimento = dataNascimento;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+}

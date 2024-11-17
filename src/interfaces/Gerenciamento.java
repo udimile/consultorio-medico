5666c557f6f5187ca8f05c1ed5861f78566ea85d
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2024-11-17 18:17:16 -0300

    implementação das interfaces (Gerenciamento e Relatório)

--- src/interfaces/Gerenciamento.java
+++ src/interfaces/Gerenciamento.java
@@ -1,7 +1,11 @@
 package interfaces;
 
-
-
 //com metodos padroes de cadastro, alteração e exclusão de registro
 public interface Gerenciamento {
+
+    public abstract void adicionar();
+
+    public abstract void atualizar();
+
+    public abstract void remover();
 }
--- src/interfaces/RelatorioInterface.java
+++ src/interfaces/RelatorioInterface.java
@@ -3,4 +3,7 @@ package interfaces;
 
 //define metodo pra geraçao de relatorios
 public interface RelatorioInterface {
+
+    public abstract void gerarRelatorio();
+
 }

e96923d6589579ec4afb6ed4d79e32e8f3af240e
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2024-11-18 17:38:37 -0300

    implementação da classe Financeiro

--- src/models/Financeiro.java
+++ src/models/Financeiro.java
@@ -1,4 +1,40 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Financeiro {
+
+    private Double valorConsulta;
+    private String formaPagamento; // cartao, dinheiro, pix
+    private String statusPagamento; // concluido, pendente
+
+    public Financeiro(Double valorConsulta, String formaPagamento, String statusPagamento) {
+        this.valorConsulta = valorConsulta;
+        this.formaPagamento = formaPagamento;
+        this.statusPagamento = statusPagamento;
+    }
+
+    public Double getValorConsulta() {
+        return valorConsulta;
+    }
+
+    public void setValorConsulta(Double valorConsulta) {
+        this.valorConsulta = valorConsulta;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public String getStatusPagamento() {
+        return statusPagamento;
+    }
+
+    public void setStatusPagamento(String statusPagamento) {
+        this.statusPagamento = statusPagamento;
+    }
 }
